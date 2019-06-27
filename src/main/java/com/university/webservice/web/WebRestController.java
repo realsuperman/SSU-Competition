cee1c7961c505d69de96b24119e9a1f9b29c2f54
shchoi <seonghun7304@naver.com>
2019-06-27 17:29:02 +0900

    commit

--- src/main/java/com/university/webservice/web/WebRestController.java
+++ src/main/java/com/university/webservice/web/WebRestController.java
@@ -1,7 +1,7 @@
 package com.university.webservice.web;
 
-import com.university.webservice.domain.posts.PostsRepository;
 import com.university.webservice.dto.posts.PostsSaveRequestDto;
+import com.university.webservice.service.PostsService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @AllArgsConstructor
 public class WebRestController {
 
-    private PostsRepository postsRepository;
+    private PostsService postsService;
 
     @GetMapping("/hello")
     public String hello() {
@@ -20,7 +20,7 @@ public class WebRestController {
     }
 
     @PostMapping("/posts")
-    public void savePosts(@RequestBody PostsSaveRequestDto dto){
-        postsRepository.save(dto.toEntity());
+    public Long savePosts(@RequestBody PostsSaveRequestDto dto){
+        return postsService.save(dto);
     }
 }
