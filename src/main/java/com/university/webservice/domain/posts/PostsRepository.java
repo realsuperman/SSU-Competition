42c428ce58dd5519ff22cdfcf1949888fa3db8c2
shchoi <seonghun7304@naver.com>
2019-06-28 17:05:14 +0900

    commit

--- src/main/java/com/university/webservice/domain/posts/PostsRepository.java
+++ src/main/java/com/university/webservice/domain/posts/PostsRepository.java
@@ -1,7 +1,13 @@
 package com.university.webservice.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.stream.Stream;
 
 public interface PostsRepository extends JpaRepository<Posts, Long> { // 이렇게하면 CRUD가 기본적으로 생김
-
+    @Query("SELECT p " +
+            "FROM Posts p " +
+            "ORDER BY p.id DESC")
+    Stream<Posts> findAllDesc();
 }
