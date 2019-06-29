66fc18223881d6470785422217c52c501b5d55b3
shchoi <seonghun7304@naver.com>
2019-06-29 21:10:41 +0900

    commit

--- src/main/java/com/university/webservice/web/WebController.java
+++ src/main/java/com/university/webservice/web/WebController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @AllArgsConstructor
-public class webController {
+public class WebController {
 
     private PostsService postsService;
 
