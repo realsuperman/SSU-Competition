1edfc4d513271c48f143698b1d9d2353402e8ed1
남기훈 <ghikhoon@naver.com>
2019-07-02 23:38:36 +0900

    중복체크 입력값 없으면 입력하라고 말해줌

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -1,3 +1,4 @@
+var duplication = 1; //중복체크
 var main = {
     init : function () {
         var _this = this;
@@ -13,10 +14,11 @@ var main = {
                 data: { userId: $('#userId').val() }
             }).done(function(data) {
                 console.log(data);
-                if(data>0) {
-                    alert("중복 입력하였습니다");
+                if(data>0){
+                    alert("이미 아이디가 존재합니다");
                 }
                 else{
+                    duplication = 0;
                     alert("사용 가능한 아이디입니다");
                 }
             }).fail(function (error) {
@@ -24,14 +26,35 @@ var main = {
             });
         });
     },
-    save : function () {
+    save : function() {
+
+        if ($('#userId').val()== "") {
+            alert("아이디를 입력해주세요");
+            return;
+        }
+        if (duplication){
+            alert("중복체크를 해주십시요");
+            return;
+        }
+
+        if ($('#userPw').val()=="") {
+            alert("비밀번호를 입력해주세요");
+            return;
+        }
+        if ($('#name').val()=="") {
+            alert("이름을 입력해주세요");
+            return;
+        }
+        if ($('#phone').val() == "") {
+            alert("휴대폰번호를 입력해주세요");
+            return;
+        }
         var data = {
             userId: $('#userId').val(),
             userPw: $('#userPw').val(),
             name: $('#name').val(),
-            phone : $('#phone').val()
+            phone: $('#phone').val()
         };
-
         $.ajax({
             type: 'POST',
             url: '/users',
